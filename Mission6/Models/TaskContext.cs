71466411ee150473e869fc0630f63ed2f81ba5a3
hwatkins23 <90345407+hwatkins23@users.noreply.github.com>
2022-02-07 11:01:49 -0700

    added models

---
+++ Mission6/Models/TaskContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext (DbContextOptions<TaskContext> options) : base(options)
+        {
+            // this can stay blank
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tasks> Tasks { get; set; }
+
+    }
+}
