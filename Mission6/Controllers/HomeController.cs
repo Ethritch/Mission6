cf7cc0a8ede5b959db0e8780324693bae94301a0
Ethritch <ethritch@gmail.com>
2022-02-07 15:38:22 -0700

    here is the controller for us to go throgh while error checking

--- Mission6/Controllers/HomeController.cs
+++ Mission6/Controllers/HomeController.cs
@@ -11,11 +11,12 @@ namespace Mission6.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private TaskContext _TContext { get; set; }
+
+        public HomeController(TaskContext passTask)
         {
-            _logger = logger;
+            _TContext = passTask;
         }
 
         public IActionResult Index()
@@ -23,6 +24,79 @@ namespace Mission6.Controllers
             return View();
         }
 
-       
+        [HttpGet]
+        public IActionResult AddTask()
+        {
+            ViewBag.Categories = _TContext.Categories.ToList();
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddTask(Tasks ar)
+        {
+            if(!ModelState.IsValid)
+            {
+                _TContext.Add(ar);
+                _TContext.SaveChanges();
+                return View();
+            }
+            else
+            {
+                ViewBag.Categories = _TContext.Categories.ToList();
+
+                return View(ar);
+            }
+            
+        }
+        [HttpGet]
+        public IActionResult ViewTasks()
+        {
+            var form = _TContext.Tasks
+                 .Include(x => x.Category)
+                 .OrderBy(x => x.Quadrent).ToList();
+            return View(form);
+
+            
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int taskid)
+        {
+            ViewBag.Categories = _TContext.Categories.ToList();
+
+            var form = _TContext.Tasks.Single(x => x.TaskId == taskid);
+
+            return View("ViewTasks", form);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(AddTask blah)
+        {
+            _TContext.Update(blah);
+            _TContext.SaveChanges();
+
+            return RedirectToAction("ViewTasks");
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int taskid)
+        {
+            var form = _TContext.Tasks.Single(x => x.TaskId == taskid);
+
+            return View(form);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Tasks ar)
+        {
+            _TContext.Tasks.Remove(ar);
+            _TContext.SaveChanges();
+
+            return RedirectToAction("ViewTasks");
+        }
+
+
+
     }
 }
