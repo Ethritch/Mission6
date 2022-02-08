0f747acc45fb289f6005104438d8f6804f54031f
jennahabel <86261801+jennahabel@users.noreply.github.com>
2022-02-07 19:11:43 -0700

    commit 8

--- Mission6/Models/TaskContext.cs
+++ Mission6/Models/TaskContext.cs
@@ -8,7 +8,7 @@ namespace Mission6.Models
 {
     public class TaskContext : DbContext
     {
-        public TaskContext (DbContextOptions<TaskContext> options) : base(options)
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
         {
             // this can stay blank
         }
@@ -16,5 +16,16 @@ namespace Mission6.Models
         public DbSet<Category> Categories { get; set; }
         public DbSet<Tasks> Tasks { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasData(
+                new Category { CategoryID = 1, CategoryName = "Home" },
+                new Category { CategoryID = 2, CategoryName = "School" },
+                new Category { CategoryID = 3, CategoryName = "Work" },
+                new Category { CategoryID = 4, CategoryName = "Church" }
+
+                );
+
+        }
     }
 }
